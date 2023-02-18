0e751319b5b43060e45c0f6cf84ddc68e565caaa
Alejandro Naranjo <alejandro.naranjo@gmail.com>
2023-02-18 00:42:44 -0500

    Added playground Config and additional content types

---
+++ config/plugin.js
@@ -0,0 +1,16 @@
+module.exports = () => ({
+    seo: {
+      enabled: true,
+    },
+    graphql: {
+      enabled: true,
+      config: {
+        playgroundAlways: true,
+        defaultLimit: 10,
+        maxLimit: 100,
+        apolloServer: {
+          tracing: true,
+        },
+      }
+    }
+  })
